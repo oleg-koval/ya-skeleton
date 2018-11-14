55e5eeb54b2336c46a8b4bb0cda4238a1a3903fc
Ed <kvl.olg@gmail.com>
2018-11-14 11:24:40 +0100

    test: Add chai

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,15 @@
 const request = require('supertest');
+const chai = require('chai');
+
 const app = require('../lib/app');
 const config = require('../config');
 
+chai.config.includeStack = true;
+chai.config.showDiff = true;
+
+global.chai = chai;
+global.expect = chai.expect;
+global.assert = chai.assert;
 global.request = request;
-global.app = app;
 global.config = config;
+global.app = app;
--- test/routes.debug.exception.js
+++ test/routes.debug.exception.js
@@ -3,7 +3,10 @@ describe('General tests, remove later', () => {
     return request(app)
       .post('/debug/test')
       .expect(200)
-      .expect('Content-Type', /json/);
+      .expect('Content-Type', /json/)
+      .then(({ body }) => {
+        expect(body.another).to.equal(1);
+      });
   });
 
   it('/debug/error: should return 500', () => {
